75fd9997abe50c55c6cc81ecbcf1f7d2a2fc5d53
Vansh Aggarwal <vanshagg233@gmail.com>
2025-06-18 19:11:15 -0700

    decoding parquet

--- src/risk_function/main.py
+++ src/risk_function/main.py
@@ -1,2 +1,5 @@
-def run():
-    print("Risk function initialized.")
+import pandas as pd
+
+df_parquet = pd.read_parquet('/Users/vanshaggarwal/Desktop/UCLA_Research/testing_location-lidar-10084636266401282188_1120_000_1140_000.parquet')
+print(df_parquet.columns)
+print(df_parquet.head())
