a98d6f0cb54119382ba3a968c0785e6bf4c82bec
Vansh Aggarwal <vanshagg233@gmail.com>
2025-06-26 13:45:40 -0700

    creating csv from incident data

--- src/risk_function/main.py
+++ src/risk_function/main.py
@@ -1,5 +1,24 @@
 import pandas as pd
 
-df_parquet = pd.read_parquet('/Users/vanshaggarwal/Desktop/UCLA_Research/testing_location-lidar-10084636266401282188_1120_000_1140_000.parquet')
-print(df_parquet.columns)
-print(df_parquet.head())
+data = pd.read_csv('/Users/vanshaggarwal/Downloads/SGO-2021-01_Incident_Reports_ADS.csv')
+
+data['VIN'] = data['VIN'].astype(str).str.strip()
+data = data[data['VIN'].notna() & (data['VIN'] != '') & (data['VIN'] != 'nan')]
+
+unique_vins = data['VIN'].unique()
+print(f'Total unique VINs: {len(unique_vins)}')
+
+all_rows = []
+
+for vin in unique_vins:
+    vin_df = data[data['VIN'] == vin]
+
+    # Add a separator row
+    separator = pd.DataFrame([['--- VIN: ' + vin + ' ---'] + [''] * (data.shape[1] - 1)], columns=data.columns)
+    all_rows.append(separator)
+
+    # Add the actual data rows
+    all_rows.append(vin_df)
+
+final_df = pd.concat(all_rows, ignore_index=True)
+final_df.to_csv('grouped_by_vin.csv', index=False)
