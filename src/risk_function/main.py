3fc113647e5e4d2af2e6b52d3882fc9124a1fff7
Vansh Aggarwal <vanshagg233@gmail.com>
2025-06-26 14:09:09 -0700

    updated to use Same Vehicle ID

--- src/risk_function/main.py
+++ src/risk_function/main.py
@@ -2,16 +2,17 @@ import pandas as pd
 
 data = pd.read_csv('/Users/vanshaggarwal/Downloads/SGO-2021-01_Incident_Reports_ADS.csv')
 
-data['VIN'] = data['VIN'].astype(str).str.strip()
-data = data[data['VIN'].notna() & (data['VIN'] != '') & (data['VIN'] != 'nan')]
+data['Same Vehicle ID'] = data['Same Vehicle ID'].astype(str).str.strip()
+data = data[data['Same Vehicle ID'].notna() & (data['Same Vehicle ID'] != '') & (data['Same Vehicle ID'] != 'nan')]
 
-unique_vins = data['VIN'].unique()
+unique_vins = data['Same Vehicle ID'].unique()
 print(f'Total unique VINs: {len(unique_vins)}')
 
 all_rows = []
 
+# Build the csv in all_rows
 for vin in unique_vins:
-    vin_df = data[data['VIN'] == vin]
+    vin_df = data[data['Same Vehicle ID'] == vin]
 
     # Add a separator row
     separator = pd.DataFrame([['--- VIN: ' + vin + ' ---'] + [''] * (data.shape[1] - 1)], columns=data.columns)
