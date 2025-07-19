1806947a260f1582802a63346837accc2933219a
Vansh Aggarwal <vanshagg233@gmail.com>
2025-07-19 12:07:21 -0700

    updating changes

--- src/risk_function/Crash_Frequency.py
+++ src/risk_function/Crash_Frequency.py
@@ -123,12 +123,47 @@ def timeOfDay(start_month, start_year, end_month, end_year):
     plt.show()
     
     
-def getMode(file, column):
+def calculate(file, column):
     data = pd.read_csv(file)
     arr = []
-    for row in data.iterrows():
-        num = str(row[column])
-        arr.append(num)
-    print(statistics.mode(arr))
-        
+    for _, row in data.iterrows():
+        try:
+            num = int(row[column])
+            arr.append(num)
+        except ValueError:
+            continue
+    return arr
+
+def getMode(file, column):
+    arr = calculate(file, column)
+    ret = str(statistics.mode(arr))
+    print('Mode: ' + ret)
+    
+def getMean(file, column):
+    arr = calculate(file, column)
+    ret = str(statistics.mean(arr))
+    print('Mean: ' + ret)
+
+def getMedian(file, column):
+    arr = calculate(file, column)
+    ret = str(statistics.median(arr))
+    print('Median: ' + ret)
+    
+def getHistogram(file, column):
+    arr = calculate(file, column)
+    counts, bins, patches = plt.hist(arr, bins=10, edgecolor='black')
+    
+    for count, left, right in zip(counts, bins[:-1], bins[1:]):
+        x = (left + right) / 2
+        y = count
+        plt.text(x, y + 0.1, str(int(count)), ha='center')
+
+    plt.xlim(0,5)
+    plt.title('Histogram')
+    plt.xlabel('Value')
+    plt.ylabel('Frequency')
+    plt.savefig('frequency_histogram.png', dpi=300)
+    plt.show()
+    
+
     
--- src/risk_function/main.py
+++ src/risk_function/main.py
@@ -2,4 +2,8 @@ import Crash_Frequency
 
 # Crash_Frequency.createSorted()
 # Crash_Frequency.getFreq()
-Crash_Frequency.timeOfDay(5, 2024, 5, 2025)
+# Crash_Frequency.timeOfDay(5, 2024, 5, 2025)
+Crash_Frequency.getMode('crash_freq.csv','Frequency')
+Crash_Frequency.getMean('crash_freq.csv','Frequency')
+Crash_Frequency.getMedian('crash_freq.csv','Frequency')
+Crash_Frequency.getHistogram('crash_freq.csv','Frequency')
